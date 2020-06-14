be8ff0a7416ebae538cfbbd821fa28bf274914de
dunglv1607 <levandung1607@gmail.com>
2020-06-14 17:24:39 +0700

    p3

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <sigup></sigup>
+    
+  </div>
+</template>
+<script>
+import sigup from './components/sigup'
+export default {
+    components:{
+        "sigup":sigup
+    }
+    
+}
+</script>
+
+
---
+++ src/components/sigup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="register">
+        <h2>ĐĂNG KÍ</h2>
+        <form @submit.prevent="register">
+          <label for="email">Email</label>
+          <br>
+          <input type="text" name="email" id="email" v-model="email" value="email" placeholder="nhập email">
+          <br>
+          <label for="password">Password</label>
+          <br>
+          <input type="text" name="password" id="password" v-model='password' value="password" placeholder="nhập password">
+          <br>
+           <button @click.prevent='btn'>Đăng Kí</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data(){
+    return{
+      
+    }
+  }
+  
+}
+</script>
+
+
+
+
+
+
+
+
+
+
